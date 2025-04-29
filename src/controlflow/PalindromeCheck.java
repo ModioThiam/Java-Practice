b872b047a5f81eb16b4a0a94e25bbd0400f1cece
modia <modio.thiam@gmail.com>
2025-04-29 02:49:54 -0400

    Starting Palindrome Check file - receiving String from user, and storing in variable.

---
+++ src/controlflow/PalindromeCheck.java
@@ -0,0 +1,15 @@
+// checks to see if a given string is a palindrome.
+package controlflow;
+
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args){
+        // get string as input
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+
+        // check to see if string is palindrome.
+
+    }
+}
